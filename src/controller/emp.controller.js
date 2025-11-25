d24c5a1afe00bc0cf104735c909d3907d3c87048
Yagnik <chandegarayagnik9@gmail.com>
2025-11-25 12:42:07 +0530

    final complete the employee table

--- src/controller/emp.controller.js
+++ src/controller/emp.controller.js
@@ -1,7 +1,63 @@
 import { dbConnection } from "../config/db.js";
 
+
+export const createEmp = async (req, res) => {
+    const { empukid, name, email, position, salary, flag = "A", phone, join_date, DepartmentID } = req.body;
+    
+    const sequelize = await dbConnection();
+    
+    try {
+        
+        if (flag === "A") {
+            const [emailCheck] = await sequelize.query(
+                `SELECT email FROM emp WHERE email = :email`,
+                { replacements: { email } }
+            );
+            
+            if (emailCheck.length > 0) {
+                return res.status(400).json({
+                    error: "Email already exists",
+                    Success: false
+                });
+            }
+        }
+        
+        let query = "";
+        
+        if (flag === "U") {
+            query += ` 
+            DELETE FROM emp WHERE empukid = :empukid;
+            `;
+        }
+        
+        query += `
+        INSERT INTO emp
+        (empukid, name, email, position, salary, flag, phone, join_date, DepartmentID)
+        VALUES
+        (:empukid, :name, :email, :position, :salary, :flag, :phone, :join_date, :DepartmentID);
+        `;
+        
+        await sequelize.query(query, {
+            replacements: {
+                empukid, name, email, position, salary, flag, phone, join_date, DepartmentID
+            },
+        });
+        
+        res.status(200).json({
+            message:
+            flag === "A" ? "Employee Create SuccessFully" : "Employee Update SuccessFully", Success: true
+        });
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.errors[0].message, Success: false });
+    } finally {
+        await sequelize.close();
+    }
+};
+
 export const getEmp = async (req, res) => {
-    const { empukid, name, position, salary, page, pageSize } = req.query
+    const { empukid, name, position, salary, page, pageSize } = req.query;
     const sequelize = await dbConnection()
     try {
 
@@ -12,7 +68,7 @@ export const getEmp = async (req, res) => {
         if (empukid) {
             query += ` AND empukid= :empukid`;
             countQuery += ` AND empukid= :empukid`;
-            replacements.empukid = empukid
+            replacements.empukid = empukid;
             console.log("Id => ", query);
         }
 
@@ -59,8 +115,6 @@ export const getEmp = async (req, res) => {
 
         const [result] = await sequelize.query(query, { replacements });
 
-        console.log("Replacement => ", replacements);
-
         res.status(200).json({ data: result, totalCount })
     } catch (error) {
         res.status(500).json({ message: error.message })
@@ -68,48 +122,7 @@ export const getEmp = async (req, res) => {
     } finally {
         await sequelize.close()
     }
-}
-
-export const createEmp = async (req, res) => {
-    const { empukid, name, email, position, salary, flag = "A", phone,  join_date, DepartmentID } = req.body;
-
-    const sequelize = await dbConnection();
-
-    try {
-
-        let query = "";
-
-        if (flag === "U") {
-            query += ` 
-        DELETE FROM emp WHERE empukid = :empukid;
-      `;
-        }
-
-        query += `
-      INSERT INTO emp
-      (empukid, name, email, position, salary, flag, phone, join_date, DepartmentID)
-      VALUES
-      (:empukid, :name, :email, :position, :salary, :flag, :phone, :join_date, :DepartmentID);
-    `;
-
-        await sequelize.query(query, {
-            replacements: {
-                empukid, name, email, position, salary, flag, phone, join_date, DepartmentID
-            },
-        });
-
-        res.status(200).json({
-            message:
-                flag === "A" ? "Employee Create SuccessFully" : "Employee Update SuccessFully", Success: true
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: err.message, Success: false });
-    } finally {
-        await sequelize.close();
-    }
-};
+} 
 
 export const deleteEmp = async (req, res) => {
     const { empukid } = req.params;
