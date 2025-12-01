bed8c4e3c5b410184f7771a83e2c6e60585ba5df
Yagnik <chandegarayagnik9@gmail.com>
2025-12-01 18:15:33 +0530

    ...

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,14 +4,14 @@ import "dotenv/config";
 
 export const createregister = async (req, res) => {
     try {
-        const { username, email, password } = req.body;
+        const { userukid, username, email, password, mobile } = req.body;
         const sequelize = await dbConnection()
-        
+
         await sequelize.query(
-            `INSERT INTO users (username, email, password) VALUES (:username, :email, :password)`,
+            `INSERT INTO users(userukid, username, email, password, Mobile ) VALUES(:userukid, :username, :email, :password, :mobile);`,
             {
                 replacements: {
-                    username, email, password
+                    userukid, username, email, password, mobile
                 }
             }
         );
@@ -38,14 +38,43 @@ export const createlogin = async (req, res) => {
         if (!password) return res.status(401).json({ error: "Invalid password" });
 
         const token = jwt.sign(
-            { id: user[0].id,
-              email: user[0].email },
+            {
+                id: user[0].id,
+                email: user[0].email
+            },
             process.env.JWT_SECRET,
             { expiresIn: "2h" }
         );
 
-        res.json({ message: "Login success", token, Success : true });
+        res.json({ message: "Login success", token, Success: true });
     } catch (err) {
-        res.status(500).json({ error: err.message, Success : false });
+        res.status(500).json({ error: err.message, Success: false });
+    }
+}
+
+export const forgetPassword = async (req, res) => {
+    const { password, mobile } = req.body
+
+    const sequelize = await dbConnection()
+
+    try {
+
+        if (!password || !mobile) {
+            return res.status(400).json({ error: "Password And Moblie Is Required", Success: false })
+        }
+
+        const result = await sequelize.query(`update users set password = :password where mobile = :moblie;`,
+            {
+                replacements: { password, mobile },
+            }
+        )   
+
+        console.log("Result => ", result);
+
+        res.status(200).json({ message: "Password Update SuccessFully" })
+
+    } catch (error) {
+        res.status(500).json({ message: "Database Error " })
+        console.log(error);
     }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createregister, createlogin } from "../controller/user.controller.js";
+import { createregister, createlogin, forgetPassword } from "../controller/user.controller.js";
 
 const router = Router()
 
 router.post("/register", createregister)
 
 router.post("/login", createlogin)
+
+router.put("/forgetpassword", forgetPassword);
+
+// router.delete("deleteuser:UserUkId", deleteUser)
 
 export default router
